0adec23bf1cb049045bd54f38ada36cfbadb06f9
satoshi.hotta <3104hotta@gmail.com>
2026-06-22 08:51:40 +0900

    ownership

--- ch04-understanding-ownership/src/bin/01_ownership.rs
+++ ch04-understanding-ownership/src/bin/01_ownership.rs
@@ -28,4 +28,23 @@ fn main() {
     s.push_str(" world!");
 
     println!("{}", s);
+
+    // https://doc.rust-jp.rs/book-ja/ch04-01-what-is-ownership.html#%E6%89%80%E6%9C%89%E6%A8%A9%E3%81%A8%E9%96%A2%E6%95%B0
+    // 所有権と関数
+
+    let s = String::from("hello");
+
+    takes_ownership(s);
+
+    let x = 5;
+
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
