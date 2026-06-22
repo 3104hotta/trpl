251a710a9d6653f6a4734083bef85f74ef0ee518
satoshi.hotta <3104hotta@gmail.com>
2026-06-23 08:59:48 +0900

    可変参照

--- ch04-understanding-ownership/src/bin/02_references_borrowing.rs
+++ ch04-understanding-ownership/src/bin/02_references_borrowing.rs
@@ -3,13 +3,25 @@
 // NG例はコメントアウト＋エラー注記で残す。
 
 fn main() {
+    // 参照と借用
     let s1 = String::from("hello");
 
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
+
+    // 可変参照
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    println!("{}", s);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
