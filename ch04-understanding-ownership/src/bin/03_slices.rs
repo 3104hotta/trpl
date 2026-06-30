011b8b789108335d6694433acce3c3fa4bf55779
satoshi.hotta <3104hotta@gmail.com>
2026-06-30 09:01:50 +0900

    add 引数としての文字列スライス

--- ch04-understanding-ownership/src/bin/03_slices.rs
+++ ch04-understanding-ownership/src/bin/03_slices.rs
@@ -16,9 +16,23 @@ fn main() {
     s.clear();
 
     println!("the first word is: {}", word);
+
+    // 引数としての文字列スライス
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(my_string_literal);
 }
 
-fn first_word(s: &String) -> usize /* &str */ {
+fn first_word(s: &str) -> usize /* &str */ {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
